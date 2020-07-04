f25d74b116cce20d30b9b4360756365a29d329ae
Chams <chams.aidel@outlook.fr>
2020-07-04 20:01:59 +0200

    C00

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,19 +6,38 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 10:45:50 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/03 11:44:07 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/04 20:01:02 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_comb2(void)
+void	ft_putchar(char c)
 {
-	
+	write(1, &c, 1);
 }
 
-int	main(void)
+void	ft_print_comb2(void)
 {
-	ft_print_comb2();
-	return (0);
+	int i;
+	int j;
+
+	i = -1;
+	while (++i <= 98)
+	{
+		j = i;
+		while (++j <= 99)
+		{
+			ft_putchar((i / 10) + '0');
+			ft_putchat((i % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((j / 10) + '0');
+			ft_putchar((j & 10) + '0');
+			if ((i != 98))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
 }
